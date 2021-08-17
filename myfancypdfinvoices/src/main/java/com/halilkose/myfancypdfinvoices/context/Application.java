72a43773db3fa4a81ed578f69feae2b00d8e793e
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-08-17 13:16:16 +0300

    Dependency Injection
    -"Inject" the userService into the InvoiceService, when creating it.

--- myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/Application.java
+++ myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/Application.java
@@ -5,8 +5,7 @@ import com.halilkose.myfancypdfinvoices.service.InvoiceService;
 import com.halilkose.myfancypdfinvoices.service.UserService;
 
 public class Application {
-    public static final InvoiceService invoiceService = new InvoiceService();
-    public static final ObjectMapper objectMapper = new ObjectMapper();
     public static final UserService userService = new UserService();
-
+    public static final InvoiceService invoiceService = new InvoiceService(userService);
+    public static final ObjectMapper objectMapper = new ObjectMapper();
 }
--- myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/service/InvoiceService.java
+++ myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/service/InvoiceService.java
@@ -9,15 +9,19 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class InvoiceService {
 
+    private final UserService userService;
     List<Invoice> invoices = new CopyOnWriteArrayList<>(); //
+
+    public InvoiceService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Invoice> findAll() {
         return invoices;
     }
 
     public Invoice create(String userId, Integer amount) {
-
-        User user = new Application().userService.findById(userId);
+        User user = userService.findById(userId);
         if (user == null){
             throw  new IllegalStateException();
         }
