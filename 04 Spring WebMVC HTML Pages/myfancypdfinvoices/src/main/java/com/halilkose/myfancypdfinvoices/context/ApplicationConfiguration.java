c40d38f425dbda21ff84a88359436f35f09cfc27
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-08-29 18:07:33 +0300

    Thymeleaf Maven Dependencies
    -thymeleaf-spring5

--- 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -5,6 +5,10 @@ import com.halilkose.myfancypdfinvoices.ApplicationLauncher;
 import org.springframework.context.annotation.*;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -21,5 +25,30 @@ public class ApplicationConfiguration {
     @Bean
     public ObjectMapper objectMapper(){
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+
+        viewResolver.setOrder(1); // optional
+        viewResolver.setViewNames(new String[] {"*.html", "*.xhtml"}); // optional
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setCacheable(false);
+        return templateResolver;
     }
 }
