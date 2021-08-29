8b8d70f11474a12d745a8606437702db8d70860d
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-08-29 17:26:37 +0300

    fix
    some fix

--- 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -2,10 +2,6 @@ package com.halilkose.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.halilkose.myfancypdfinvoices.ApplicationLauncher;
-import com.halilkose.myfancypdfinvoices.service.InvoiceService;
-import com.halilkose.myfancypdfinvoices.service.UserService;
-import org.hibernate.validator.internal.engine.MethodValidationConfiguration;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
