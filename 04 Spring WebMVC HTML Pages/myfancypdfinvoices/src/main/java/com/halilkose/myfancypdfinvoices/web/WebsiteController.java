cdf3ad51e8ee82be1773fd0254f7bf9d2129c393
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-08-29 18:28:51 +0300

    Thymeleaf Templates & Models
    -Creating more demanding Thymeleaf templates
    -Filling the model

--- 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/web/WebsiteController.java
+++ 04 Spring WebMVC HTML Pages/myfancypdfinvoices/src/main/java/com/halilkose/myfancypdfinvoices/web/WebsiteController.java
@@ -1,12 +1,18 @@
 package com.halilkose.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
     @GetMapping("/")
-    public String homepage(){
+    public String homepage(Model model, @RequestParam(required = false,defaultValue = "stranger")String username){
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate",new Date());
         return "index.html";
     }
 }
