4c86e3b6d897fdce07f5693884204466dc2ba8c8
Halil İbrahim Köse <halilkosee5@gmail.com>
2021-08-31 02:24:54 +0300

    Spring Boot Basics init
    -Creating a new project ( https://start.spring.io/)
    Inspecting Spring Boot projects
     *.gitignore
     *Embedded Maven
     *Checkpoint: Maven Wrapper
     *Spring Boot pom.xml file
     *mvn dependency:tree
     *@SpringBootApplication
    -

---
+++ 06 Spring Boot Basics/myfancypdfinvoices-springboot/src/main/java/com/halilkose/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.halilkose.myfancypdfinvoices.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfancypdfinvoicesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfancypdfinvoicesSpringbootApplication.class, args);
+	}
+
+}
---
+++ 06 Spring Boot Basics/myfancypdfinvoices-springboot/src/test/java/com/halilkose/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halilkose.myfancypdfinvoices.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
